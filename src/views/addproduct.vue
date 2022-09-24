56e78b5d53a2c25d848864a600e9fa12e05b6929
markmojang <markmojang555@gmail.com>
2022-09-24 10:15:14 +0700

    updated

--- src/views/addproduct.vue
+++ src/views/addproduct.vue
@@ -5,26 +5,31 @@
                 <div class="col-sm-8 offset-sm-2">
                     <div>
                         <h2 class="text-center text-bg-primary">เพิ่มสินค้า</h2>
-                        <form @submit.prevent="handleAddProduct">
-                            <div class="form-group">
-                                <label for="store">เพิ่มสินค้า</label>
-                                <input type="text" placeholder="ชื่อสินค้า" class="form-control"
-                                    v-model="product.name" />
-                            </div>
 
-                            <div class="form-group">
-                                <label for="address">ราคา</label>
-                                <input type="text" placeholder="ราคา" class="form-control" v-model="product.price" />
-                            </div>
+                        <div class="form-group">
+                            <label for="store">เพิ่มสินค้า</label>
+                            <input type="text" placeholder="ชื่อสินค้า" class="form-control" v-model="product.name" />
+                        </div>
 
-                            <div class="form-group">
-                                <label for="">Image</label>
-                                <input type="file" class="form-control" @change="onFileSelected" />
-                            </div>
-                            <button class="btn btn-primary m-1" @click="handleProduct">
-                                เพิ่ม
-                            </button>
-                        </form>
+                        <div class="form-group">
+                            <label for="address">ราคา</label>
+                            <input type="text" placeholder="ราคา" class="form-control" v-model="product.price" />
+                        </div>
+                        <div class="form-group">
+                            <label for="address">รายละเอียด</label>
+                            <input type="text" placeholder="รายละเอียด" class="form-control" v-model="product.detail" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Image Link</label>
+                            <input type="text" placeholder="link" class="form-control" v-model="product.image" />
+
+                        </div>
+                        <!-- Button -->
+                        <button class="btn btn-primary m-1" @click="handleAddProduct">
+                            เพิ่ม
+                        </button>
+
                     </div>
                 </div>
             </div>
@@ -47,18 +52,18 @@ export default {
         };
     },
     methods: {
-        onFileSelected(e) {
-            this.product.image = e.target.files[0];
-            console.log(e.target.files[0]);
-        },
         async handleAddProduct() {
             {
-                var product = new FormData();
-                product.append("name", this.product.name);
-                product.append("detail", this.product.detail);
-                product.append("imageUpload", this.product.image);
-                product.append("price", this.product.price);
-                await axios.post("https://v-backend-shop.herokuapp.com/product", {product})
+                const body = {
+                    name: this.product.name,
+                    price: this.product.price,
+                    image: this.product.image,
+                    detail: this.product.detail,
+                }
+
+                const res = await axios.post("https://v-shop-backend.herokuapp.com/product", body)
+                console.log(res);
+                this.$router.push("/")  // เปลี่ยนหน้า
             }
         },
     },
